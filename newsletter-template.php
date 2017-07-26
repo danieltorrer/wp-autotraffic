42a19799b5f7a8cbc3bf93c42005289f79f7fbe5
Daniel Alejandro Torres <daniel.torrer@gmail.com>
2017-07-25 20:03:05 -0500

    newsletter

--- newsletter-template.php
+++ newsletter-template.php
@@ -27,12 +27,12 @@
             </style>
             <div id="mc_embed_signup row">
               <!-- <div class="col-md-6"> -->
-              <form action="//github.us11.list-manage.com/subscribe/post?u=1afcc2a1248b0c7b4a94c087f&amp;id=86cbea8591"
+              <form action="//autotraffic.us15.list-manage.com/subscribe/post?u=f9f0f3d7072c43e238cc8f342&amp;id=9d25e5ac4c"
                 method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate no-smoothState" target="_blank" novalidate>
                   <div id="mc_embed_signup_scroll form-group">
               	     <input type="email" value="" name="EMAIL" class="email form-control bg placeholder" id="mce-EMAIL" placeholder="Escribe tu correo" required>
                      <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                     <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_1afcc2a1248b0c7b4a94c087f_86cbea8591" tabindex="-1" value=""></div>
+                     <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_f9f0f3d7072c43e238cc8f342_9d25e5ac4c" tabindex="-1" value=""></div>
                      <div class="clear">
                        <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button no-smoothState">
                        <img class="icon-form" src="<?php echo get_template_directory_uri(); ?>/images/send-icon.png" alt="#">
